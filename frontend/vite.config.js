1a36a8083dffde486614f14f3dde431759ebee41
meaceofpind <bergeatharva@gmail.com>
2024-11-29 08:13:58 +0530

    Changes to server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 10000,
+    host: '0.0.0.0',
     proxy: {
       '/api/v1': {
         target: process.env.REACT_APP_API_BASE_URL,
